652b941818fd12a539373614c4ad75fb558957c1
Jason Sabeniano <35453938+Sabeniano@users.noreply.github.com>
2024-01-26 15:12:40 +0100

    Update mqtt.py

--- Raspberry PI/IoHT/mqtt.py
+++ Raspberry PI/IoHT/mqtt.py
@@ -1,62 +1,85 @@
-import os
-import sqlite3
+import folium
+import threading
 import encryption
-import paho.mqtt.client as mqtt
+from mqtt import mqtt_listener
+from models import Bruger, Koordinater, Promille, Faldet
+from flask import Flask, g, render_template, request, url_for, redirect, session
 
-db_path = db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'db', 'phDB.db'))
+#Starter mqtt som lytter på gps_data_topic
+thread = threading.Thread(target=mqtt_listener())
+thread.start()
 
-def on_message(client, userdata, message):
-    data = message.payload.decode("utf-8")
+app = Flask(__name__, static_folder='static')
+app.secret_key = 'SDSDSHFT23213213FDSFSDF'
 
-    parts = data.split(" ")
-    prod_koord = parts[0]
-    lat_long = parts[1].split(",", 1)
-    fall_status = parts[2]
-    promille = parts[3]
+#Funktion der starter hver gang der er en HTTP request
+@app.before_request
+def before_request():
+    if 'produkt_id' in session:
+        produkt_id = session['produkt_id']
+        user = Bruger.find_by_produkt_id(produkt_id)
+        if user:
+            g.user = user
 
+@app.route("/")
+def index():
+     return render_template('index.html')
 
-    encrypted_latitude = encryption.encrypt_data(lat_long[0])
-    encrypted_longitude = encryption.encrypt_data(lat_long[1])
+#TODO opret
+@app.route("/registrer", methods=['GET', 'POST'])
+def registrer():
+    if request.method == 'POST':
+        registrer = request.form.get('produkt_id')
+    else:
+        return render_template('index.html')
 
-    conn = sqlite3.connect(db_path)
-    cur = conn.cursor()
-    cur.execute('''
-                UPDATE koordinater
-                SET latitude = ?, longitude = ?
-                WHERE bruger_id = (
-                    SELECT bruger_id
-                    FROM bruger
-                    WHERE produkt_id = ?
-                )
-                ''', (encrypted_latitude, encrypted_longitude, prod_koord))
+@app.route("/index", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        produkt_id = request.form.get('produkt_id')
 
-    cur.execute('''
-                    UPDATE faldet
-                    SET faldet_status = ?
-                    WHERE bruger_id = (
-                        SELECT bruger_id
-                        FROM bruger
-                        WHERE produkt_id = ?
-                    )
-                    ''', (fall_status, prod_koord))
+        produkt = Bruger.find_by_produkt_id(produkt_id)
+        if produkt:
+            session['produkt_id'] = produkt.produkt_id
+            return redirect(url_for('profil'))
+        else:
+            error = 'Produkt ID not found in database'
+            return render_template('index.html', error=error)
+    else:
+        return render_template('index.html')
     
-    cur.execute('''
-                    UPDATE promille
-                    SET promille = ?
-                    WHERE bruger_id = (
-                        SELECT bruger_id
-                        FROM bruger
-                        WHERE produkt_id = ?
-                    )
-                    ''', (promille, prod_koord))
-    conn.commit()
-    conn.close()
-    print("record inserted.")
-    print(f"Received message: {message.payload.decode()}")
+@app.route("/profil")
+def profil():
+    if g.user:
+        produkt_id = g.user.produkt_id
+        koordinater = Koordinater.find_by_produkt_id(produkt_id)
+        promille = Promille.find_by_produkt_id(produkt_id)
+        faldet = Faldet.find_by_produkt_id(produkt_id)
 
-def mqtt_listener():
-    client = mqtt.Client()
-    client.on_message = on_message
-    client.connect("192.168.4.2", 1883)
-    client.subscribe("gps_data_topic")
-    client.loop_start()
+        latitude, longitude = 55.69178, 12.55388  # Default location
+        if koordinater:
+            latitude = encryption.decrypt_data(koordinater.latitude)
+            longitude = encryption.decrypt_data(koordinater.longitude)
+            print(latitude, longitude)
+
+        # Create the map centered on the user's location
+        m = folium.Map(location=[latitude, longitude], zoom_start=13)
+        folium.Marker(location=[latitude, longitude], popup='Lokation').add_to(m)
+
+        # Render the profil.html with user information and the map
+        return render_template('profil.html', user=g.user, map=m._repr_html_(), promille=promille, faldet=faldet)
+    else:
+        return redirect(url_for('index'))
+
+@app.route("/dashboard")
+def dashboard():
+    ##TODO Liste af alle brugere, hvor man bliver notificeret
+    return render_template('dashboard.html')
+
+@app.route("/logout")
+def logout():
+    session.pop('produkt_id', None)
+    return redirect(url_for('index'))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
